dc41952cd20fbac44597ca86baa5b47b300407a0
ragohin <ragohin@gmail.com>
2018-08-08 22:45:14 -0400

    updated array.cpp

--- array/array.cpp
+++ array/array.cpp
@@ -1,1 +1,20 @@
 using namespace std;
+
+template <typename T>
+
+class Array{
+
+
+public:
+
+
+private:
+    T* arr; //array can contain any type T which points to the first element in the array
+    int arr_size
+};
+
+template <typename T>
+Array<T>::Array(const int& size_in){ //constructor for array
+    arr = new T[size_in];
+    size = size_in;
+}
